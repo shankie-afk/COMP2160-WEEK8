0af2882dc3a812943dbd9a78ae479aab6efe638f
Noah Penza <penzanoah@gmail.com>
2021-10-06 14:41:05 +1100

    Update Coin.cs

--- Week 8 Task/Assets/Scripts/Coin.cs
+++ Week 8 Task/Assets/Scripts/Coin.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public int playerScore;
     ScoreKeeper scoreKeeperScript;
-
-    // Start is called before the first frame update
     void Start()
     {
        GameObject scoreKeeper = GameObject.Find("ScoreKeeper");
